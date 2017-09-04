890d17fad4b92f9b09bc4ae7f28847b42b8e2bcd
Joseph <Joseph@Laptop_of_Joe.paci.us>
2017-09-04 10:49:01 -0400

    Fixed a typo

--- src/geobot/GeoBot.java
+++ src/geobot/GeoBot.java
@@ -16,7 +16,7 @@ public class GeoBot {
      */
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Will this show up to me in netbeans?")
+        System.out.println("Will this show up to me in netbeans?");
     }
     
 }
